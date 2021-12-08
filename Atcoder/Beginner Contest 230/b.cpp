1e48b71bbe942a1f1baf475604d1cc1a640c956d
Subhadeep Paul <deepsp2001@gmail.com>
2021-12-08 08:11:55 +0530

    Code

---
+++ Atcoder/Beginner Contest 230/b.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ff(i,a,n) for(int(i) = int(a);i <= int(n);i++)
+#define fb(i,a,n) for(int(i) = int(a);i >= int(n);i--)
+#define lli long long int
+#define vi vector<int>
+#define vlli vector<lli>
+#define pb push_back
+#define vii vector<vi>
+#define pii pair<int,int>
+#define endl "\n"
+#define mp make_pair
+#define fix(a,b) memset(a,b,sizeof(a))
+#define iter(i,a) for( typeof(a.begin()) i=a.begin();i!=a.end();i++)
+//lli mod = 1000000007;
+
+using namespace std;
+
+void solve()
+{
+    string str, str1;
+    cin >> str1;
+    str = "oxxoxxoxxoxxoxxoxx";
+    size_t found = str.find(str1);
+    cout << (found != string::npos ? "Yes" : "No");
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+	int t = 1;
+	//cin >> t;
+	while(t--)
+        solve();
+	return 0;
+}
